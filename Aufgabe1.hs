9245b33560b73fb3b27e8721d855900523516cbe
Eduard <eduard.thamm@thammit.at>
2011-10-12 17:04:11 +0200

    Added Comments

--- Aufgabe1.hs
+++ Aufgabe1.hs
@@ -23,31 +23,30 @@ type NumberOf = Integer
 
 variations :: Integer -> Integer -> Integer
 variations m r
-    | m<r || r<0 || m<0 = (-1)
-    | otherwise = binom (m,r) * fac r
+    | m<r || r<0 || m<0 = (-1) -- check for "error" cases 
+    | otherwise = binom (m,r) * fac r -- if none of the error cases are true use the mathematicl formula
 
 
 numberOfOcc :: Symbol -> Text -> NumberOf
-numberOfOcc a (b:bs)
-    | a==b = 1+(numberOfOcc a bs)
-    | a/=b = 0+(numberOfOcc a bs)
-numberOfOcc a [] = 0
+numberOfOcc a (b:bs) 
+    | a==b = 1+(numberOfOcc a bs) --add one and call recursion
+    | a/=b = 0+(numberOfOcc a bs) -- on unequal dont add anything and recurse
+numberOfOcc a [] = 0 --this is needed for structure recognition the recursion must know what to do with the empty word
 
 
 
 mostCommonSymbol :: Text -> Symbol
 mostCommonSymbol [] = error "Program error: Result\n"
-{-mostCommonSymbol = error "Program error: kein Resultat\n"
--}
+
 
 -- helper functions for variations
 
-binom :: (Integer,Integer) -> Integer
+binom :: (Integer,Integer) -> Integer --one needs binomial coefficient to calculate the number of variations this code was discussed in the lecture
 binom (n,k)
     | k==0 || n==k = 1
     | otherwise = binom (n-1,k-1) + binom (n-1,k)
 
-fac :: Integer -> Integer
+fac :: Integer -> Integer -- one needs the factorial function to calculate the number of variations this cado was also discussed in the lecture
 fac n
     | n == 0 = 1
     | n > 0 = n * fac (n - 1)
