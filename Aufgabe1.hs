762a354e2c4c75db931c3036ea3929338d89bf89
Eduard <eduard.thamm@thammit.at>
2011-10-12 15:08:15 +0200

    Implement variations

--- Aufgabe1.hs
+++ Aufgabe1.hs
@@ -10,16 +10,37 @@ erstellt von:
 -}
 
 
-pick :: Integer -> [Integer] -> [Integer]
+--pick :: Integer -> [Integer] -> [Integer]
 
 
-pickAll :: [Integer] -> [Integer] -> [Integer]
+--pickAll :: [Integer] -> [Integer] -> [Integer]
 
+
+-- Typedefs as taken form specification
+type Symbol = Char
+type Text = String
+type NumberOf = Integer
 
 variations :: Integer -> Integer -> Integer
+variations m r
+    | m<r || r<0 || m<0 = (-1)
+    | otherwise = binom (m,r) * fac r
+
+--numberOfOcc :: Symbol -> Text -> NumberOf
 
 
-numberOfOcc :: Symbol -> Text -> NumberOf
+--mostCommonSymbol :: Text -> Symbol
 
 
-mostCommonSymbol :: Text -> Symbol
+-- helper functions for variations
+
+binom :: (Integer,Integer) -> Integer
+binom (n,k)
+    | k==0 || n==k = 1
+    | otherwise = binom (n-1,k-1) + binom (n-1,k)
+
+fac :: Integer -> Integer
+fac n
+    | n == 0 = 1
+    | n > 0 = n * fac (n - 1)
+    | otherwise = error "Nur positive Argumente!"
