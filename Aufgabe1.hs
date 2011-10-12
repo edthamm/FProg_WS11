e0a1f22866a705fd6f6e270c0490326be511a8a1
Eduard <eduard.thamm@thammit.at>
2011-10-12 14:38:42 +0200

    Add File and Signatures

---
+++ Aufgabe1.hs
@@ -0,0 +1,25 @@
+{-
+############################################################
+
+Aufgabe1.hs
+erstellt von:
+) Matthias Krug
+) Eduard Thamm 0525087
+
+############################################################
+-}
+
+
+pick :: Integer -> [Integer] -> [Integer]
+
+
+pickAll :: [Integer] -> [Integer] -> [Integer]
+
+
+variations :: Integer -> Integer -> Integer
+
+
+numberOfOcc :: Symbol -> Text -> NumberOf
+
+
+mostCommonSymbol :: Text -> Symbol
