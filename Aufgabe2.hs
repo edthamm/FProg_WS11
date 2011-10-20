ef75c33b5053260778cf08c9bc3c4838f5ee48cb
Eduard <eduard.thamm@thammit.at>
2011-10-20 08:19:20 +0200

    Add some Code sucheAlle

--- Aufgabe2.hs
+++ Aufgabe2.hs
@@ -32,7 +32,16 @@ suche (e:es) (s:ss)
     | take (length (s:ss)) (e:es) /= (s:ss) = (suche es (s:ss)) + 1
     | otherwise = 0
 
+{-
+sucheAlle :: Editor ->Suchzeichenreihe -> [Index]
+sucheAlle (e:es) (s:ss)
+    | not (isInfixOf (s:ss) (e:es)) = []
+    | partialsuche /= -1 = [partialsuche]:[ x + length (s:ss)| x <- (sucheAlle (stripPräfix (subtext (e:es) (s:ss))) (s:ss))] CC idea check if the first length s elements fit if yes add to list, need to add lentgh of s times number of striped beginnings
+    | otherwise = [] CC :[ x + length (s:ss)| x <- (sucheAlle (stripPräfix (subtext (e:es) (s:ss))) (s:ss))]
+    where 
+        partialsuche = subsuche (subtext (e:es) (s:ss))
+        subsuche a b = suche a b
+        subtext (e:es) (s:ss) = take (length (s:ss)) (e:es)-}
 
---sucheAlle :: Editor ->Suchzeichenreihe -> [Index]
 
 --ersetze :: Editor ->Vorkommen -> Alt -> Neu -> Editor
