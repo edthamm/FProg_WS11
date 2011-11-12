00d82ba2e84fed0518ce9094db62fe36e9f2af96
Eduard <eduard.thamm@thammit.at>
2011-11-12 11:22:41 +0100

    implement 4 and 5

--- Aufgabe5.hs
+++ Aufgabe5.hs
@@ -21,22 +21,32 @@ any (((<)2).length.lines) ctxlines
 -}
 
 -- Part2
-unixtac :: String -> String
+{-unixtac :: String -> String
 unixhead :: Int -> String -> String
 unixtail :: Int -> String -> String
 unixgrep :: String -> String -> String
+-}
 
 -- Part3
+{-
 aslines :: ([String]->[String]) -> String -> String
 unixtac' :: String -> String
 unixhead' :: Int -> String -> String
 unixtail' :: Int -> String -> String
 unixgrep' :: String -> String -> String
+-}
 
 -- Part4
 unixrev :: String -> String
-wordrev :: String -> String
+unixrev s = unlines$map reverse (lines(s))
 
+wordrev :: String -> String
+wordrev s = unwords$map reverse (words(s))
 -- Part5
 unixwcw :: String -> Int
+unixwcw s = length$words(s)
+
 unixwc :: String -> (Int,Int,Int)
+unixwc s = (length$lines(s) ,words, symbols)
+    where words = unixwcw s
+          symbols = length s
