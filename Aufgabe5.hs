3cdde1a1e4eac32a18a47966e54a5658fb81fe99
Matthias <matthias.krug@student.tuwien.ac.at>
2011-11-16 10:42:54 +0100

    added some comments

--- Aufgabe5.hs
+++ Aufgabe5.hs
@@ -29,27 +29,32 @@ ctxunwords = [["hallo","hi\n"], ["a\n","b","c"], ["hal\nlo"],["a    b"]]
 
 -- Part2
 
+-- Hängt Zeilen in umgekehrter Richtung aneinander
 unixtac :: String -> String
 unixtac s 
     | (isSuffixOf "\n" s) = unlines$reverse$lines s
-    | otherwise = _replaceFirstNewLine $unlines$reverse$lines s
+    | otherwise = _replaceFirstNewLine $unlines$reverse$lines s -- Sonderfall falls der String am Ende kein \n enthält
 
+-- erstes Vorkommen von \n entfernen
 _replaceFirstNewLine :: String -> String
 _replaceFirstNewLine "" = ""
 _replaceFirstNewLine (s:xs)
     | [s] == "\n" = xs
     | otherwise = s : (_replaceFirstNewLine xs)
 
+-- Liefert die ersten n Zeilen eines Strings
 unixhead :: Int -> String -> String
 unixhead i s
     | (i == (length $lines s)) && (isSuffixOf "\n" s) == False = reverse$tail$reverse$unlines$take i $lines s
     | otherwise = unlines$take i $lines s
 
+-- Liefert die letzten n Zeilen eines Strings
 unixtail :: Int -> String -> String
 unixtail i s
     | (isSuffixOf "\n" s) == False = reverse$tail$reverse$unlines$reverse$take i $reverse$lines s
     | otherwise = unlines$reverse$take i $reverse$lines s
 
+-- Liefert alle Zeilen des Strings die einen bestimmten Substring enthalten
 unixgrep :: String -> String -> String
 unixgrep "" qs = unlines$lines qs -- grep gibt jedes gefundene mit newline am ende aus
 unixgrep ss qs = unlines$ [a| a <- b , isInfixOf ss a]
@@ -57,43 +62,51 @@ unixgrep ss qs = unlines$ [a| a <- b , isInfixOf ss a]
 
 -- Part3
 
+-- das Gemeinsame aus den Funktionen von Part2 herausgehoben
 aslines :: ([String]->[String]) -> String -> String
 aslines f s = unlines$f $lines s
 
-
+-- wie in Part2 nur mit aslines
 unixtac' :: String -> String
 unixtac' s
     | (isSuffixOf "\n" s) = aslines reverse s
     | otherwise = _replaceFirstNewLine $aslines reverse s
 
+		-- wie in Part2 nur mit aslines
 unixhead' :: Int -> String -> String
 unixhead' i s
     | (i == (length $lines s)) && (isSuffixOf "\n" s) == False = reverse$tail$reverse$aslines (take i) s
     | otherwise = aslines (take i) s
 
+		-- wie in Part2 nur mit aslines
 unixtail' :: Int -> String -> String
 unixtail' i s 
     | (isSuffixOf "\n" s) == True = aslines (drop((length(lines s)) - i)) s
     | otherwise = reverse$tail$reverse$aslines (drop((length(lines s)) - i)) s
 
+		-- wie in Part2 nur mit aslines
 unixgrep' :: String -> String -> String
 unixgrep' "" qs = aslines (\x->x) qs -- grep gibt jedes gefundene mit newline am ende aus 
 unixgrep' ss qs = aslines (filter (isInfixOf ss)) qs
  
 
 -- Part4
+-- einen String zeilenweise invertieren
 unixrev :: String -> String
 unixrev s = unlines$map reverse (lines(s))
 
+-- einen String wortweise invertieren
 wordrev :: String -> String
 wordrev s = unlines$map unwords $map reverse $map words $lines s
 
 
 
 -- Part5
+-- Anzahl der Woerter eines Strings ausgeben
 unixwcw :: String -> Int
 unixwcw s = length$words(s)
 
+-- Anzahl der Zeilen, Woerter und Zeichen eines Strings ausgeben
 unixwc :: String -> (Int,Int,Int)
 unixwc s = (length$lines(s) ,words, symbols)
     where words = unixwcw s
