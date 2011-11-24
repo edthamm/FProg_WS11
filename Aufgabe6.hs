9b0d6e2fd4f03c7dcce3547fd02dfcf421ddeff3
Eduard <eduard.thamm@thammit.at>
2011-11-24 15:52:24 +0100

    Add Aufgabe6.hs / Signatures

---
+++ Aufgabe6.hs
@@ -0,0 +1,36 @@
+module Aufgabe6 where
+
+import Data.List
+import Debug.Trace
+
+{-
+############################################################
+
+Aufgabe6.hs
+erstellt von:
+) Matthias Krug 0828965
+) Eduard Thamm 0525087
+
+############################################################
+-}
+
+--Typedefs to spec
+data DOrd = Infix | Praefix | Postfix |
+            GInfix | GPraefix | GPostfix
+data BTree = Nil |
+             BNode Int BTree BTree deriving (Eq,Ord,Show)
+type Control = String
+type Func = Integer -> Integer
+type Data = Integer
+data Tree = Leaf Func
+          | Node Func Tree Tree Tree
+data LTree = LNode Integer [LTree] deriving Show
+
+
+--flatten :: BTree -> DOrd -> [Int]
+
+--isST :: BTree -> Bool
+
+--apply :: Control -> Data -> Tree -> Integer
+
+--mapLT :: Func -> LTree -> LTree
