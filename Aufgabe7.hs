710e355186bbc3170840fdc28d0b86175660ab69
Eduard <eduard.thamm@thammit.at>
2011-12-07 09:57:25 +0100

    add typedefs

--- Aufgabe7.hs
+++ Aufgabe7.hs
@@ -12,4 +12,31 @@ erstellt von:
 ############################################################
 -}
 
+--typedefs
+type Vertex = Integer
+type Origin = Vertex
+type Destination = Vertex
+type Key = Integer
+type Name = Integer
+type State = Integer
+type StartState = State
+type AcceptingStates = [State]
+type Word a = [a]
+type Row a = [[a]]
+type Automaton a = AMgraph a
 
+
+data AMgraph a = AMg [(Row a)] deriving (Eq,Show)
+data BTree a = BLeaf Key a |
+               BNode Key a (BTree a) (BTree a) deriving Show
+data LTree a = LNode Key a [(LTree a)] deriving Show
+data ALgraph = ALg [(Origin,[Destination])] deriving (Eq,Show)
+
+
+--classdefs
+
+class Structure s where
+    noOfSources :: s -> Integer
+    noOfSinks :: s -> Integer
+    notSourceConnected :: s -> [Name]
+    notSinkConnected :: s -> [Name]
