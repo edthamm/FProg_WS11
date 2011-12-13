0cfe9051e8c6fcf963860bf5f06c87b4ab4ae79e
Matthias <matthias.krug@student.tuwien.ac.at>
2011-12-13 22:00:18 +0100

    added signaures

--- Aufgabe8.hs
+++ Aufgabe8.hs
@@ -11,3 +11,43 @@ erstellt von:
 
 ############################################################
 -}
+
+-- types
+type State = Integer
+type StartState = State
+type AcceptingStates = [State]
+type Word a = [a]
+type Row a = [[a]]
+data AMgraph a = AMg [(Row a)] deriving (Eq,Show)
+type Automaton a = AMgraph a
+
+-- types for part 1
+type Postfix a = Word a
+
+-- types for part 2
+type Prefix a = Word a
+
+-- types for part 3
+type Skyscraperline = [Integer]
+type Length = Integer
+type VisFromLeft = Integer
+type VisFromRight = Integer
+
+-- part 1
+
+isPostfix :: Eq a => (Automaton a) -> StartState -> AcceptingStates -> (Postfix a) -> Bool
+
+-- part 2
+givePrefix :: Eq a => (Automaton a) -> StartState -> AcceptingStates -> (Postfix a) -> (Maybe (Prefix a))
+
+-- part 3
+-- (a)
+isValid :: Skyscraperline -> Bool
+-- (b)
+computeVisibility :: Skyscraperline -> Skyscraperline
+-- (c)
+buildSkyscrapers :: Length -> VisFromLeft -> VisFromRight -> Maybe Skyscraperline
+-- (d)
+noOfSkyscraperLines :: Length -> VisFromLeft -> VisFromRight -> Integer
+-- (e)
+allSkyscraperLines :: Length -> VisFromLeft -> VisFromRight -> [Skyscraperline]
