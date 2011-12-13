0fd59d53987563792856931fe3aabf6f2aa31db2
Matthias <matthias.krug@student.tuwien.ac.at>
2011-12-13 23:16:57 +0100

    completed part 3 not sure if subpart c is correct - testcase is right anyway ;)

--- Aufgabe8.hs
+++ Aufgabe8.hs
@@ -41,33 +41,29 @@ type VisFromRight = Integer
 --givePrefix :: Eq a => (Automaton a) -> StartState -> AcceptingStates -> (Postfix a) -> (Maybe (Prefix a))
 
 -- part 3
--- (a)
+type Count = Integer       -- buildings count
+type Highest = Integer     -- the (currently) highest (found) building
+type Vis = Integer         -- the visibility - don't care which site
+
+-- (a) checks if a given skycraperline is valid
 isValid :: Skyscraperline -> Bool
 isValid line = (_checkLeft left content) && (_checkRight right content) && (_checkContent content)
     where left = head line
           right = last line
           content = reverse $ tail $ reverse $ tail line
 
-type Count = Integer
-type Highest = Integer
-
 -- check the left view
 _checkLeft :: VisFromLeft -> Skyscraperline -> Bool
-_checkLeft left line = _checkLeft_ left 0 0 line
-_checkLeft_ :: VisFromLeft -> Count -> Highest -> Skyscraperline -> Bool
-_checkLeft_ left count _ [] = left == count
-_checkLeft_ left count highest (x:xs)
-    | x > highest = _checkLeft_ left (count+1) x xs
-    | otherwise = _checkLeft_ left count highest xs
-
+_checkLeft left line = _checkSide left 0 0 line
 -- check the right view
 _checkRight :: VisFromRight -> Skyscraperline -> Bool
-_checkRight right line = _checkRight_ right 0 0 (reverse line)
-_checkRight_ :: VisFromRight -> Count -> Highest -> Skyscraperline -> Bool
-_checkRight_ right count _ [] = right == count
-_checkRight_ right count highest (x:xs)
-    | x > highest = _checkRight_ right (count+1) x xs
-    | otherwise = _checkRight_ right count highest xs
+_checkRight right line = _checkSide right 0 0 (reverse line)
+-- check either side (calculates from left to right)
+_checkSide :: Vis -> Count -> Highest -> Skyscraperline -> Bool
+_checkSide vis count _ [] = vis == count
+_checkSide vis count highest (x:xs)
+    | x > highest = _checkSide vis (count+1) x xs
+    | otherwise = _checkSide vis count highest xs
 
 -- check if the "content" is right: only 10, 20, 30, ..., n*10 allowed
 _checkContent :: Skyscraperline -> Bool
@@ -78,18 +74,45 @@ _length :: [a] -> Integer
 _length [] =  0
 _length(x:xs) = 1 + _length xs
 
--- (b)
---computeVisibility :: Skyscraperline -> Skyscraperline
--- (c)
---buildSkyscrapers :: Length -> VisFromLeft -> VisFromRight -> Maybe Skyscraperline
--- (d)
---noOfSkyscraperLines :: Length -> VisFromLeft -> VisFromRight -> Integer
--- (e)
---allSkyscraperLines :: Length -> VisFromLeft -> VisFromRight -> [Skyscraperline]
+-- (b) computates the visibility values for a scyscraper line
+computeVisibility :: Skyscraperline -> Skyscraperline
+computeVisibility line = [_computeLeftVisibility line] ++ line ++ [_computeRightVisibility line]
 
+-- computate left site
+_computeLeftVisibility :: Skyscraperline -> VisFromLeft
+_computeLeftVisibility line = _computeVisibility 0 0 line
+-- computate right site
+_computeRightVisibility :: Skyscraperline -> VisFromRight
+_computeRightVisibility line = _computeVisibility 0 0 (reverse line)
+-- computate either side (calculates from left to right)
+_computeVisibility :: Count -> Highest -> Skyscraperline -> Vis
+_computeVisibility count _ [] = count
+_computeVisibility count highest (x:xs)
+    | x > highest = _computeVisibility (count+1) x xs
+    | otherwise = _computeVisibility count highest xs
+
+-- (c) try to build a skyscraper line which fulfills the requirements
+buildSkyscrapers :: Length -> VisFromLeft -> VisFromRight -> Maybe Skyscraperline
+buildSkyscrapers l left right
+    | res == [] = Nothing
+    | otherwise = Just (head res)
+    where res = allSkyscraperLines l left right
+
+-- (d) count all possible skyscraperlines for a given length and visibility
+noOfSkyscraperLines :: Length -> VisFromLeft -> VisFromRight -> Integer
+noOfSkyscraperLines l left right = _length (allSkyscraperLines l left right)
+
+-- (e) list all possible skyscraperlines for a given length and visibility - sorted lexicographical
+allSkyscraperLines :: Length -> VisFromLeft -> VisFromRight -> [Skyscraperline]
+allSkyscraperLines l left right = _filterLines (_buildAllLines l) left right
+
+-- filter all skyscraper lines which do not fulfill the requirements
+_filterLines :: [Skyscraperline] -> VisFromLeft -> VisFromRight -> [Skyscraperline]
+_filterLines slines left right = [x | x <- slines, ((head x) == left) && ((head $ reverse x) == right)]
+-- build all lines for a given length
 _buildAllLines :: Length -> [Skyscraperline]
-_buildAllLines l = _permutation [x*10 | x <- [1..l]]
-
+_buildAllLines l = [computeVisibility y | y <- _permutation [x*10 | x <- [1..l]]]
+-- permutate a given list
 _permutation :: Skyscraperline -> [Skyscraperline]
 _permutation [] = [[]]
 _permutation xs = [x:ys | x <- xs, ys <- _permutation (delete x xs)]
