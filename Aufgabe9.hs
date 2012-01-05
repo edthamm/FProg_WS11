65c2054a6816bd834006664b3e486ea85963a311
Eduard <eduard.thamm@thammit.at>
2012-01-05 14:51:06 +0100

    add signatures

--- Aufgabe9.hs
+++ Aufgabe9.hs
@@ -11,3 +11,29 @@ erstellt von:
 
 ############################################################
 -}
+
+-- Part 1
+
+type Row = [Integer]
+type Skyline = [Row]
+
+
+isValidSL :: Skyline -> Bool
+isValidSL _ = True
+
+
+compVisibility :: Skyline -> Skyline
+compVisibility a = a
+
+--buildSkyscrapers :: Skyline -> Maybe Skyline
+
+
+-- Part 2
+
+type Sudoku = [Row]
+data Variant = Basic | Cross | Color deriving (Eq,Show)
+
+isValidSDK :: Sudoku -> Variant -> Bool
+isValidSDK _ _ = True
+
+--solve :: Sudoku -> Variant -> Maybe Sudoku
